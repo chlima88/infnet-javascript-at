46da2a9e36653d7c2e3e44afa06a21ee2048de72
Charles Costa <chlima88@gmail.com>
2022-06-30 03:00:55 -0300

    first release

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const app = {
   firstCard: 0,
   clickedCardsCount: 0,
   correctCardsCount: 0,
+  pairsCount: 8,
   timer: {
     milli: 0,
     seconds: 0,
@@ -42,7 +43,7 @@ const app = {
         this.milli = elapsedTime % 1000
         this.seconds = Math.floor((elapsedTime / 1000) % 60)
         this.minutes = Math.floor((elapsedTime / 1000) / 60)
-        this.element.innerText = `Lap Time: ${this.formatTime()}`
+        this.element.innerText = `Time: ${this.formatTime()}`
       }, 1)
     }
   },
@@ -53,16 +54,25 @@ const app = {
   },
 
   setHall: function() {
-    const newLapPlayer = this.buildElement({type: "p", classname: "hall__player-name"})
-    newLapPlayer.innerText = this.playername
-    const newLapTime = this.buildElement({type: "p", classname: "hall__player-time"})
-    newLapTime.innerText = document.querySelector(".pannel__title").innerText
-    const newLap = this.buildElement({type: "div", classname: "hall__player"})
-    newLap.append(newLapPlayer, newLapTime)
+    const playerTime = document.querySelector(".controls__title").innerText
+
+    const newLap = this.buildHallLap({name: this.playername, time: playerTime})
+    const currentTimeText = playerTime.split(' ')[1].slice(0, -1)
+    const currentTime = new Date(`1 0:${currentTimeText}`)
+
+    const scoreboard =  document.querySelectorAll(".hall__player-time")
+    const beatenTime = Array.prototype.find.call(scoreboard, scoreboardLap => {
+      const scoreboardLapText = scoreboardLap.innerText.split(' ')[1].slice(0, -1)
+      const scoreboardLapTime = new Date(`1 0:${scoreboardLapText}`)
+      return scoreboardLapTime > currentTime
+    })
 
     const hall = document.querySelector(".hall")
-    // hall.append(newLap)
-    hall.insertBefore(newLap, hall.children[1])
+    beatenTime
+      ? hall.insertBefore(newLap, beatenTime.parentNode)
+      : hall.appendChild(newLap)
+
+    hall.childElementCount >= 11 && hall.removeChild(hall.lastChild)
 
   },
 
@@ -83,12 +93,17 @@ const app = {
         this.timer.stop()
         alert("Congratulations! You won!")
         this.setHall()
+        this.gameState = "stopped"
+        this.saveGame()
       }
     }, 1000)
   },
 
   clickCard: function(card) {
-    if ( !card.classList.contains("card--correct") && ++this.clickedCardsCount <= 2 && card != this.firstCard ) {
+    if ( !card.classList.contains("card--correct")
+    && ++this.clickedCardsCount <= 2
+    && card != this.firstCard
+    && this.gameState != "loading" ) {
       if ( this.firstCard ) {
         this.updateCardState(card, "card--initial", "card--clicked")
         this.checkCards(this.firstCard, card)
@@ -111,60 +126,71 @@ const app = {
     const cardFront = this.buildElement({type: "div", classname: "card-front"})
     const cardBack = this.buildElement({type: "div", classname: "card-back"})
     cardBack.style.backgroundImage = `url('../images/${this.teamsList[pair]}.jpg')`
-    const card = this.buildElement({type: "div", classname: "card", eventListener: () => { if (this.gameState === "started") this.clickCard(card) }})
+
+    const card = this.buildElement({
+      type: "div",
+      classname: "card",
+      eventListener: () => { this.gameState === "started" && this.clickCard(card) }})
     card.pair = pair
     card.id = id
     card.append(cardFront, cardBack)
+
     const cardBox = this.buildElement({type: "div", classname: "card-box"})
     cardBox.appendChild(card)
-    this.cardList.push(card)
     return cardBox
   },
 
-  buildControls: function () {
+  buildPannel: function () {
     const buttonStart = this.buildElement({
       type: "button",
+      classname: "controls__button",
       text: "Start Lap",
       eventListener: () => {this.start()}
     })
     const buttonQuit = this.buildElement({
       type: "button",
-      text: "Restart",
+      classname: "controls__button",
+      text: "Reset Game",
       eventListener: () => {this.reset()}
     })
-    const lapTime = this.buildElement({type: "p", classname: "pannel__title", text: "Lap Time: 0:00.000s"})
+    const lapTime = this.buildElement({type: "p", classname: "controls__title", text: "Lap Time: 0:00.000s"})
     this.timer.setElement(lapTime)
-    const pannel = this.buildElement({type: "div", classname: "pannel"})
-    pannel.append(lapTime, buttonStart, buttonQuit)
+    const controls = this.buildElement({type: "div", classname: "controls"})
+    controls.append(lapTime, buttonStart, buttonQuit)
 
-    const hall = this.buildElement({type: "div", classname: "hall"})
-    const hallTitle = this.buildElement({type: "h2", classname: "hall__title", text: "Last players"})
-    const player = this.buildElement({type: "div", classname: "hall__player"})
-    const playerName = this.buildElement({type: "p", classname: "hall__player-name", text: "Charles"})
-    const playerTime = this.buildElement({type: "p", classname: "hall__player-time", text: "Lap Time: 0:30.000s"})
-
-    player.append(playerName, playerTime)
-    hall.append(hallTitle, player)
+    const hall = this.buildElement({type: "ul", classname: "hall"})
+    const gameTitle = this.buildElement({type: "h1", classname: "game__title", text: "F1 Memory"})
+    const hallTitle = this.buildElement({type: "h2", classname: "hall__title", text: "Hall of Fame"})
+    // hall.append(hallTitle)
 
     const sidebar = this.buildElement({type: "div", classname: "sidebar"})
-    sidebar.append(hall, pannel)
+    sidebar.append(gameTitle, hallTitle, hall, controls)
     return sidebar
   },
 
-  buildBoard: function (pairs=8) {
-    this.pairsCount = pairs
-    const cardList = this.generateCardList()
+  buildHallLap: function({name, time}) {
+    const playerName = this.buildElement({type: "p", classname: "hall__player-name"})
+    playerName.innerText = name
+    const playerTime = this.buildElement({type: "p", classname: "hall__player-time"})
+    playerTime.innerText = time
+    const newLap = this.buildElement({type: "li", classname: "hall__player"})
+    newLap.append(playerName, playerTime)
+    return newLap
+  },
+
+  buildBoard: function (pairs=this.pairsCount) {
+    const cardList = this.generateCardList(pairs)
     const board = this.buildElement({type: "div", classname: "board"})
     board.id = "board"
     board.append(...cardList)
     return board
   },
 
-  generateCardList: function() {
+  generateCardList: function(pairs) {
     const cardList = []
-    for (let pair = 0; pair < this.pairsCount; pair ++) {
+    for (let pair = 0; pair < pairs; pair ++) {
       cardList.push(this.buildCard({id: pair, pair}))
-      cardList.push(this.buildCard({id: pair + this.pairsCount , pair}))
+      cardList.push(this.buildCard({id: pair + pairs , pair}))
     }
     const randomizedCardList = new Set()
     while (randomizedCardList.size != cardList.length) {
@@ -176,12 +202,17 @@ const app = {
 
   start: function () {
     if (this.gameState === "stopped"){
-      this.playername = prompt("Enter you name:")
-      this.gameState = "started"
+      this.reset()
+      this.playername = prompt("Enter your name:").slice(0,12) || "Player"
+      this.gameState = "loading"
       this.timer.start()
-      this.cardList.forEach(card => {
+      const cardList = document.querySelectorAll(".card")
+      Array.prototype.forEach.call(cardList, card => {
         this.updateCardState(card, "card--initial", "card--clicked")
-        setTimeout(() => this.updateCardState(card, "card--clicked", "card--initial"), 3000)
+        setTimeout(() => {
+          this.updateCardState(card, "card--clicked", "card--initial")
+          this.gameState = "started"
+        }, 3000)
       })
     }
   },
@@ -189,7 +220,6 @@ const app = {
   reset: function() {
     this.timer.stop()
     this.gameState = "stopped"
-    this.cardList = []
     this.clickedCard = 0
     this.correctCardsCount = 0
     this.clickedCardsCount = 0
@@ -199,11 +229,34 @@ const app = {
     component.replaceChild(board, oldBoard)
   },
 
+  saveGame: function () {
+    const scoreboard =  document.querySelectorAll(".hall__player")
+    const gameData = Array.prototype.map.call(scoreboard, data =>  ({
+        name: data.children[0].innerText,
+        time: data.children[1].innerText
+      }))
+
+    localStorage.setItem("f1memory", JSON.stringify(gameData))
+  },
+
+  restoreGame: function() {
+    const gameData = JSON.parse(localStorage.getItem("f1memory"))
+    const scoreboard = document.querySelector(".hall")
+
+    if (gameData){
+      gameData.map(data =>  {
+        const restoredLap = this.buildHallLap({name: data.name, time: data.time})
+        scoreboard.appendChild(restoredLap)
+      })
+    }
+  },
+
   render: function () {
     const component = document.getElementById("app")
-    const controls = this.buildControls()
+    const pannel = this.buildPannel()
     const board = this.buildBoard()
-    component.append(controls, board)
+    component.append(pannel, board)
+    this.restoreGame()
   }
 };
 
